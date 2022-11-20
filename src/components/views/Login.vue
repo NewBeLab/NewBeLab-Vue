afc7fd5a49032b5fcc31c1192f4af2619e6fbc77
nyohoo <nyohoo2013@gmail.com>
2022-11-20 15:15:16 +0900

    add: ログインページの作成

---
+++ src/components/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+        <v-card class="elevation-1 pa-3">
+          <v-btn :href="githubUrl">
+            GitHubログイン
+          </v-btn>
+          <v-btn :href="testUrl">
+            テストログイン
+          </v-btn>
+        </v-card>
+    </v-layout>
+  </v-container>
+</template>
+ 
+<script>
+  export default {
+    name: 'Login',
+    data: () => ({
+      github: {
+        url: "http://localhost:3000/api/v1/auth/github",
+        redirectUrl: "http://localhost:8080/callback"
+      },
+      testUrl: "http://localhost:3000/api/v1/auth/sessions"
+    }),
+    computed: {
+      githubUrl() {
+        return `${this.github.url}?auth_origin_url=${this.github.redirectUrl}`
+      }
+    }
+  }
+</script>

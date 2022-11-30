a79a3283e61c165096813895b1e07ce0d6c5914b
nyohoo <nyohoo2013@gmail.com>
2022-12-01 04:51:54 +0900

    fix: ログインページをcompotisonAPI化

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -2,31 +2,20 @@
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
         <v-card class="elevation-1 pa-3">
-          <v-btn :href="githubUrl">
+          <v-btn :href="GITHUB_URL">
             GitHubログイン
-          </v-btn>
-          <v-btn :href="testUrl">
-            テストログイン
           </v-btn>
         </v-card>
     </v-layout>
   </v-container>
 </template>
  
-<script>
-  export default {
-    name: 'Login',
-    data: () => ({
-      github: {
-        url: "http://localhost:3000/api/v1/auth/github",
-        redirectUrl: "http://localhost:8080/callback"
-      },
-      testUrl: "http://localhost:3000/api/v1/auth/sessions"
-    }),
-    computed: {
-      githubUrl() {
-        return `${this.github.url}?auth_origin_url=${this.github.redirectUrl}`
-      }
-    }
-  }
+<script setup>
+// 認証用のURLを定数として定義し、v-btnのhref属性にバインドする
+// 認証に成功すると、callbackページにリダイレクトされる
+const GITHUB_URL = 
+  import.meta.env.VITE_BASE_URL + 
+  'auth/github?auth_origin_url=' + 
+  import.meta.env.VITE_SERVICE_URL + 'callback'
+
 </script>

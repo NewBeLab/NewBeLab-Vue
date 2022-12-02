e7abde7d5b22716f8c94182cf8c35c23ade113c7
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-12-02 18:06:37 +0900

    change: cookieの初期化処理を移動

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,13 @@
     <router-view />
   </v-app>
 </template>
+
+<script setup>
+import { onMounted, inject } from "vue";
+// setCookieを使うために、cookie.jsをインポート
+import { setCookie } from "@/plugins/cookie";
+
+onMounted(() => {
+  setCookie();
+});
+</script>
--- src/components/views/RouterTestFirst.vue
+++ src/components/views/RouterTestFirst.vue
@@ -2,17 +2,7 @@
   <div>
     <h1>test1</h1>
     <router-link to="/second">次のページ</router-link>
-    <br>
+    <br />
     <router-link to="/login">ログイン</router-link>
   </div>
 </template>
-
-<script setup>
-import { onMounted, inject } from 'vue'
-// setCookieを使うために、cookie.jsをインポート
-import { setCookie } from '@/plugins/cookie'
-
-onMounted( () => {
-  setCookie();
-});
-</script>

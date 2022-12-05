c3ed3f49909af19d7dbb7f94529f6ef22b332f21
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-12-05 16:49:45 +0900

    Remove: テストページの削除

--- src/components/views/RouterTestFirst.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>test1</h1>
-    <router-link to="/second">次のページ</router-link>
-    <br />
-    <router-link to="/login">ログイン</router-link>
-  </div>
-</template>
--- src/components/views/RouterTestSecond.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>test2</h1>
-    <router-link to="/">前のページ</router-link>
-  </div>
-</template>

99d754855a42f5bf4a43c9e7815d14d4132dea94
nyohoo <nyohoo2013@gmail.com>
2022-12-05 15:51:10 +0900

    add: ログイン画面の作成

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -1,12 +1,50 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-        <v-card class="elevation-1 pa-3">
-          <v-btn :href="GITHUB_URL">
-            GitHubログイン
-          </v-btn>
+  <v-container 
+    fluid 
+    fill-height
+  >
+      <v-row 
+        justify="center" 
+        align-content="center" 
+        style="height: 500px;"
+      >
+        <v-card 
+          class="elevation-12" 
+          width="40vh"
+          rounded
+        >
+          <v-toolbar 
+            color="rgba(252,92,108,0.15)"
+            dark 
+            flat
+          >
+            <v-toolbar-title 
+              class="flex text-center"
+            >
+              ログイン/ 新規登録
+            </v-toolbar-title>
+          </v-toolbar>
+
+          <v-row justify="center" class="pt-7 mb-12">
+            <iframe src="https://giphy.com/embed/l2QZSDxJmnQKc16a4" width="100%" height="35%" style="position:absolute" frameBorder="0" class="giphy-embed"></iframe>
+          </v-row>
+          <v-card-text>
+            <!-- ログインボタン -->
+            <v-btn
+              :href="GITHUB_URL"
+              color="rgba(252,92,108,0.15)"
+              block
+              large
+              rounded
+              class="white--text mb-5 mt-10"
+            >
+              <v-icon size="large">mdi-github</v-icon>
+              Login with Github
+            </v-btn>
+          </v-card-text>
+
         </v-card>
-    </v-layout>
+      </v-row>
   </v-container>
 </template>
  

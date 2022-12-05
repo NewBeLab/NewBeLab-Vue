11e43abf4d36c895d045e3992c6495839229148a
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-12-05 16:47:58 +0900

    Add: Topにログインボタンを追加

--- src/components/views/Top.vue
+++ src/components/views/Top.vue
@@ -3,14 +3,22 @@
   <v-main class="bg-grey-lighten-3">
     <v-container>
       <h1>Top</h1>
+      <div v-if="!loggedIn"><LoginButton /></div>
     </v-container>
   </v-main>
 </template>
 
-<script>
+<script setup>
+import { computed } from "vue";
+import { useAuthStore } from "@/stores/auth";
 import Header from "@/components/parts/Header.vue";
+import LoginButton from "@/components/parts/LoginButton.vue";
 
-export default {
-  components: { Header },
-};
+const authStore = useAuthStore();
+
+// ログイン中かどうかを返す算出プロパティ
+// ログイン中であればtrueを返す
+const loggedIn = computed(() => {
+  return authStore.user !== null;
+});
 </script>

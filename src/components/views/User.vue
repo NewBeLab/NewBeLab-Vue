a108906fec10ef247bec849ca57a38194fe253ce
nyohoo <nyohoo2013@gmail.com>
2022-12-12 15:22:44 +0900

    add: userページの候補２（横表示）

--- src/components/views/User.vue
+++ src/components/views/User.vue
@@ -1,38 +1,47 @@
 <template>
   <Header />
   <v-main class="bg-grey-lighten-3">
-    <v-container>
+    <v-container fill-height>
       <p class="text-h4 text-center my-12" style="color: blueviolet;">🏋️おともだち候補ズ🏋️</p>
 
-      <v-row>
-        <v-col v-for="user in users" :key="user" cols="12">
-          <v-card>
-            <v-card-title class="ml-2">
-              <!-- アバターイメージ・名前・グレードの表示部分 -->
+      <v-row jusityf-center>
+        <v-col v-for="user in users" :key="user" cols="12" jusityf-center>
+          <!-- アバターイメージ・名前・グレードの表示部分 -->
+          <v-row justify="between">
+            <v-col cols="3" md="2" sm="3" xs="1" class="pl-16 mr-0 pr-0" align-self="center">
+              <v-avatar size="120">
+                <img :src="user.image" width="120" height="120" />
+              </v-avatar>
+            </v-col>
+            
+            <!-- ディバイダー -->
+            <v-divider vertical class="mb-2 mt-12" />
+            <!-- <v-spacer /> -->
+
+            <v-col cols="10" md="9" sm="9" xs="5" class="pl-12">
+              <p class="text-h5 ml-2 mt-4 mb-3">{{ user.name }}
+                <span>（{{ user.profile.grade }}期）</span>
+              </p>
+              <!-- profile情報 -->
               <v-row>
-                <v-col cols="3" md="1" sm="2" xs="2" class="mt-1 pr-0">
-                  <v-avatar size="60">
-                    <img :src="user.image" />
-                  </v-avatar>
+                <v-col cols="6">
+                  <p class="text-h6">可能なコミット　：{{ user.profile.commitment }}</p>
                 </v-col>
-                <v-col cols="8" md="11" sm="10" xs="10" class="pl-0">
-                  <h3 class="ml-2 mt-4">{{ user.name }} 
-                    <span>（{{ user.profile.grade }}期）</span>
-                  </h3>
+                <v-col cols="6">
+                  <p class="text-h6">希望ポジション　：{{ user.profile.position }}</p>
                 </v-col>
               </v-row>
-            </v-card-title>
-            <!-- ディバイダー -->
-            <v-divider insent class="mx-3 mt-3" />
-            <!-- profile情報 -->
-            <v-card-text>
-              <p class="text-h6">可能なコミット　：{{ user.profile.commitment }}</p>
-              <p class="text-h6">希望ポジション　：{{ user.profile.position }}</p>
-              <p class="text-h6">チーム開発の動機：{{ user.profile.motivation }}</p>
-              <p class="text-h6">学習フェーズ　　：{{ user.profile.phase }}</p>
-              <p class="text-h6">使用エディター　：{{ user.profile.editor }}</p>
-            </v-card-text>
-          </v-card>
+              <v-row>
+                <v-col cols="6">
+                  <p class="text-h6">チーム開発の動機：{{ user.profile.motivation }}</p>
+                </v-col>
+                <v-col cols="6">
+                  <p class="text-h6">学習フェーズ　　：{{ user.profile.phase }}</p>
+                </v-col>
+              </v-row>
+            </v-col>
+
+          </v-row>
         </v-col>
       </v-row>
     </v-container>

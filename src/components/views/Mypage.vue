56528cfbc9cdc248d241e93909d54094b6cf86ee
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-12-15 12:11:38 +0900

    Add: マイページにプロフィールの表示を追加

--- src/components/views/Mypage.vue
+++ src/components/views/Mypage.vue
@@ -3,12 +3,82 @@
   <v-main class="bg-grey-lighten-3">
     <v-container>
       <Alert />
-      <h1>Mypage</h1>
+      <h1>マイページ</h1>
+      <v-avatar color="grey-darken-1" size="144">
+        <img
+          :src="!!authStore.user ? authStore.user.image : ''"
+          width="144"
+          height="144"
+        />
+      </v-avatar>
+      <v-card elevation="2">
+        <v-card-title>
+          {{ authStore.user.name }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{
+            !!myProfile.selfIntroduction
+              ? myProfile.selfIntroduction
+              : "自己紹介文が設定されていません"
+          }}
+        </v-card-subtitle>
+        <v-card-text>
+          <div>
+            モチベーション：{{
+              !!myProfile.motivation ? myProfile.motivation : "未設定"
+            }}
+          </div>
+          <div>
+            学習フェーズ：{{ !!myProfile.phase ? myProfile.phase : "未設定" }}
+          </div>
+          <div>
+            チーム開発にコミットできる度合い：{{
+              !!myProfile.commitment ? myProfile.commitment : "未設定"
+            }}
+          </div>
+          <div>
+            希望ポジション：{{
+              !!myProfile.position ? myProfile.position : "未設定"
+            }}
+          </div>
+          <div>
+            好きなエディター：{{
+              !!myProfile.editor ? myProfile.editor : "未設定"
+            }}
+          </div>
+          <div>
+            グレード：{{ !!myProfile.grade ? myProfile.grade : "未設定" }}
+          </div>
+          <div>
+            Mattermost：{{ !!myProfile.grade ? myProfile.grade : "未設定" }}
+          </div>
+        </v-card-text>
+      </v-card>
+      <v-btn :to="{ name: 'Profile' }" elevation="3">プロフィール編集</v-btn>
     </v-container>
   </v-main>
 </template>
 
 <script setup>
+import { inject, onMounted, ref } from "vue";
 import Header from "@/components/parts/Header.vue";
 import Alert from "@/components/parts/Alert.vue";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+
+const myProfile = ref([]);
+
+onMounted(() => {
+  // 自分のプロフィール情報を取得
+  const axios = inject("axios");
+  axios
+    .get(`/profiles/${authStore.user.id}`)
+    .then((response) => {
+      myProfile.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 </script>

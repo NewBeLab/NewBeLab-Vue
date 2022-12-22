79c452704be19a96c5cba121d8c6cd762f0ba9b3
Minako Yoshino(MBA) <minakoyoshino3@gmail.com>
2022-12-22 23:27:33 +0900

    Add: トップページのデザインを追加

--- src/components/views/Top.vue
+++ src/components/views/Top.vue
@@ -1,10 +1,31 @@
 <template>
   <Header />
   <v-main>
-    <v-container>
-      <div class="text-h3">Top</div>
-      <div v-if="!loggedIn"><LoginButton /></div>
-    </v-container>
+    <div id="top-image">
+      <v-container>
+        <v-row align-content="center" style="min-height: 800px">
+          <v-col align="center">
+            <div class="text-h3">見出し見出し見出し</div>
+            <div class="text-subtitle-1">
+              この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を
+            </div>
+            <div v-if="!loggedIn"><LoginButton /></div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+    <div id="top-color">
+      <v-container>
+        <v-row align-content="center" style="min-height: 800px">
+          <v-col align="center">
+            <div class="text-h3">見出し見出し見出し</div>
+            <div class="text-subtitle-1">
+              この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </v-main>
 </template>
 
@@ -22,3 +43,15 @@ const loggedIn = computed(() => {
   return authStore.user !== null;
 });
 </script>
+
+<style>
+#top-image {
+  background-image: url("/AdobeStock_262216616.jpg");
+  background-color: rgba(255, 255, 255, 0.9);
+  background-blend-mode: lighten;
+  background-size: cover;
+}
+#top-color {
+  background: #ffe6e9;
+}
+</style>
